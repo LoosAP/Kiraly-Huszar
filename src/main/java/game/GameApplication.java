3424ed7091456d53d5cc11938ee53ea5118ef5ba
Loós András Péter <loosandraspeter@gmail.com>
2023-05-08 10:25:33 +0200

    implemente GameApplication and Main methods

--- src/main/java/game/GameApplication.java
+++ src/main/java/game/GameApplication.java
@@ -1,4 +1,19 @@
 package game;
 
-public class GameApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GameApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/game.fxml"));
+        stage.setTitle("Király és huszár kihívás");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 }
