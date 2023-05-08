80cffb959f50c4a284eb1222aea9202fd8b3edf6
Loós András Péter <loosandraspeter@gmail.com>
2023-05-08 11:31:23 +0200

    added icon to the window

--- src/main/java/game/GameApplication.java
+++ src/main/java/game/GameApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class GameApplication extends Application {
@@ -11,7 +12,9 @@ public class GameApplication extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/game.fxml"));
         stage.setTitle("Király és huszár kihívás");
+        Image icon = new Image("/knighticon.png");
         Scene scene = new Scene(root);
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
