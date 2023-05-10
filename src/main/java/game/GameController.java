1b7e5c4bb34d51844f5b82ed3e91aaefcb61a7cb
Loós András Péter <loosandraspeter@gmail.com>
2023-05-10 19:30:49 +0200

    added generic GameController code, will need to expand on it later

--- src/main/java/game/GameController.java
+++ src/main/java/game/GameController.java
@@ -1,4 +1,43 @@
 package game;
 
+import game.model.GameModel;
+import javafx.beans.binding.ObjectBinding;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
+import static game.model.SquareStates.*;
+
 public class GameController {
+
+    @FXML
+    private GridPane board;
+
+    private GameModel model = new GameModel();
+
+    @FXML
+    private void initialize(){
+        for (var i = 0; i < board.getRowCount(); i++) {
+            for (var j = 0; j < board.getColumnCount(); j++) {
+                var square = createSquare(i, j);
+                board.add(square, j, i);
+            }
+        }
+    }
+    private StackPane createSquare(int i, int j) {
+        return null;
+    }
+    @FXML
+    private void handleMouseClick(MouseEvent event) {
+        var square = (StackPane) event.getSource();
+        var row = GridPane.getRowIndex(square);
+        var col = GridPane.getColumnIndex(square);
+        System.out.printf("Click on square (%d,%d)%n", row, col);
+        model.move(row, col);
+    }
+
 }
