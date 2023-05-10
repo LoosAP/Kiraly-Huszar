f76a87ecb1ca88f3b2d9ff3bccd05931b25ba459
Loós András Péter <loosandraspeter@gmail.com>
2023-05-10 21:16:25 +0200

    implemented constructor, and functions to get the board's parameters

--- src/main/java/game/model/GameModel.java
+++ src/main/java/game/model/GameModel.java
@@ -1,4 +1,30 @@
 package game.model;
 
+import javafx.beans.Observable;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+
 public class GameModel {
+
+    public static int SIZE = 8;
+
+    private ReadOnlyObjectWrapper<SquareStates>[][] board = new ReadOnlyObjectWrapper[SIZE][SIZE];
+    public GameModel() {
+        for (var i = 0; i < SIZE; i++) {
+            for (var j = 0; j < SIZE; j++) {
+                board[i][j] = new ReadOnlyObjectWrapper<SquareStates>(SquareStates.NONE);
+            }
+        }
+    }
+
+    public ReadOnlyObjectProperty<SquareStates> squareProperty(int i, int j) {
+        return board[i][j].getReadOnlyProperty();
+    }
+
+    public SquareStates getSquareState(int i, int j){
+        return board[i][j].get();
+    }
+
+    public void move(Integer row, Integer col) {
+    }
 }
