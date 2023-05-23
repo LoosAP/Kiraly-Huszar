e76da7e5191c90a44212e4f518c17013d814e3aa
Loós András Péter <loosandraspeter@gmail.com>
2023-05-23 21:05:38 +0200

    added redo function

--- src/main/java/game/GameController.java
+++ src/main/java/game/GameController.java
@@ -34,6 +34,9 @@ public class GameController {
     // tracks the moves made by the player
     ArrayList<MoveTracker> undoTracker = new ArrayList<>();
 
+    // tracks the moves made by undoTracker
+    ArrayList<MoveTracker> redoTracker = new ArrayList<>();
+
     @FXML
     private void initialize(){
         for (var i = 0; i < board.getRowCount(); i++) {
@@ -92,6 +95,7 @@ public class GameController {
         if (newSelectedState == NONE && selectedPiece != null){
             if (model.canMovePiece(row,col,selectedPiece)){
                 undoTracker.add(new MoveTracker(row,col,selectedPiece,model.getRow(selectedPiece),model.getCol(selectedPiece)));
+                redoTracker.clear();
                 model.move(row,col,selectedPiece);
 
                 selectedPiece = null;
@@ -142,6 +146,7 @@ public class GameController {
         // reset variables
         selectedPiece = null;
         undoTracker.clear();
+        redoTracker.clear();
         model.clearBoard();
 
         // Generating random numbers
@@ -190,16 +195,37 @@ public class GameController {
     }
 
     public void onUndo(ActionEvent actionEvent) {
-       //gets the last element of the moveTracker arraylist
-        MoveTracker lastMove = undoTracker.get(undoTracker.size()-1);
-        //moves the piece back to its previous position
-        model.move(lastMove.getPrevRow(),lastMove.getPrevCol(),lastMove.getPrevPiece());
-        //removes the last element of the arraylist
-        undoTracker.remove(undoTracker.size()-1);
+        try{
+            //gets the last element of the moveTracker arraylist
+            MoveTracker lastMove = undoTracker.get(undoTracker.size()-1);
+            //moves the piece back to its previous position
+            model.move(lastMove.getPrevRow(),lastMove.getPrevCol(),lastMove.getPrevPiece());
+            // adds the last move to redoTracker
+            redoTracker.add(lastMove);
+            //removes the last element of the arraylist
+            undoTracker.remove(undoTracker.size()-1);
+        }
+        catch (Exception e){
+            System.out.println("No more moves to undo");
+        }
+
 
     }
 
     public void onRedo(ActionEvent actionEvent) {
+        try{
+            //gets the last element of the redoTracker arraylist
+            MoveTracker lastMove = redoTracker.get(redoTracker.size()-1);
+            //moves the piece back to its previous position
+            model.move(lastMove.getCurrentRow(),lastMove.getCurrentCol(),lastMove.getPrevPiece());
+            // adds the last move to undoTracker
+            undoTracker.add(lastMove);
+            //removes the last element of the arraylist
+            redoTracker.remove(redoTracker.size()-1);
+        }
+        catch (Exception e){
+            System.out.println("No more moves to redo");
+        }
     }
 
     public void onHints(ActionEvent actionEvent) {
