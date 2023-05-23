12602531e93558e689f0d0f6e2a0e11a28269506
Loós András Péter <loosandraspeter@gmail.com>
2023-05-23 21:13:27 +0200

    preventing default close request

--- src/main/java/game/GameApplication.java
+++ src/main/java/game/GameApplication.java
@@ -1,12 +1,17 @@
 package game;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.util.Optional;
 
 public class GameApplication extends Application {
     @Override
@@ -19,5 +24,21 @@ public class GameApplication extends Application {
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+        stage.setOnCloseRequest(e -> {
+            e.consume();
+            closeProgram(stage);
+        });
     }
+
+    private void closeProgram(Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText("Are you sure you want to exit?");
+        alert.setContentText("All unsaved progress will be lost");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            Platform.exit();
+        }
+    }
+
 }
